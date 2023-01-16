fcdb109d196bcad3bc4d482070cdc67f4907923c
sizkeOne <gonzaloamoletto@live.com.ar>
2023-01-15 23:05:17 -0300

    Agregado de backend

---
+++ ProyectoFinalMoletto/BackEnd/src/main/java/com/portfolio/molettogonzalo/MolettogonzaloApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.molettogonzalo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolettogonzaloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolettogonzaloApplication.class, args);
+	}
+
+}
---
+++ ProyectoFinalMoletto/BackEnd/src/test/java/com/portfolio/molettogonzalo/MolettogonzaloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.molettogonzalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MolettogonzaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
